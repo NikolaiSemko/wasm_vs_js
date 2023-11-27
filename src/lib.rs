6f988bd8efb3913b2b72c6230a9b683ca87ddf41
NikolaiSemko <nisemko@gmail.com>
2023-11-27 01:45:40 -0500

    first version

---
+++ src/lib.rs
@@ -0,0 +1,112 @@
+use std::mem;
+use std::slice;
+use std::os::raw::c_void;
+
+
+#[no_mangle]
+pub extern "C" fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
+    unsafe {
+        let _buf = Vec::from_raw_parts(ptr, 0, cap);
+    }
+}
+
+// #[no_mangle]
+// pub extern "C" fn fill(pointer: *mut u8, max_width: usize, max_height: usize, time: f64) {
+//     let byte_size = max_width * max_height * 4;
+//     let sl = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
+//
+//     for i in 0..byte_size {
+//
+//         // get the position of current pixel
+//         let height = i / 4 / max_width;
+//         let width = i / 4 % max_width;
+//
+//         if i % 4 == 3 {
+//
+//             // set opacity to 1
+//             sl[i] = 255;
+//         } else if i % 4 == 0 {
+//
+//             // create a red ripple effect from the top left corner
+//             let len = ((height * height + width * width) as f64).sqrt();
+//             let nb = time + len / 4.0;
+//             let a = 128.0 + nb.cos() * 128.0;
+//             sl[i] = a as u8;
+//         } else if i % 4 == 2 {
+//
+//             // create a blue ripple effect from the top right corner
+//             let width = 500 - width;
+//             let len = ((height * height + width * width) as f64).sqrt();
+//             let nb = time + len / 4.0;
+//             let a = 128.0 + nb.cos() * 128.0;
+//             sl[i] = a as u8;
+//         }
+//     }
+// }
+
+#[no_mangle]
+pub extern "C" fn fill(pointer: *mut u8, w: usize, h: usize, _n: usize, j: u32, simple_mod: bool) {
+    let byte_size = w * h * 4;
+    let delta_h = ((25800.0 / (w as f64 * 4.0)).ceil()) as usize;
+
+    let byte_size2 = w * h * 4 + delta_h * w * 4;
+    let y2 = byte_size + 25600;
+    let sl = unsafe { slice::from_raw_parts_mut(pointer, byte_size2) };
+    if simple_mod {
+        for y in 0..h {
+            for x in 0..w {
+                let pos = 4 * (y * w + x);
+                if sl[pos + 2] == 255 { sl[pos + 3] = sl[pos + 3] & 0xFB; }
+                if sl[pos + 2] == 0 { sl[pos + 3] = sl[pos + 3] | 0x04; }
+                if (sl[pos + 3] & 0x04) > 0 { sl[pos + 2] += 1; } else { sl[pos + 2] -= 1; }
+                if sl[pos + 1] == 255 { sl[pos + 3] = sl[pos + 3] & 0xFD; }
+                if sl[pos + 1] == 0 { sl[pos + 3] = sl[pos + 3] | 0x02; }
+                if (sl[pos + 3] & 0x02) > 0 { sl[pos + 1] += 1; } else { sl[pos + 1] -= 1; }
+                if sl[pos] == 255 { sl[pos + 3] = sl[pos + 3] & 0xFE; }
+                if sl[pos] == 0 { sl[pos + 3] = sl[pos + 3] | 0x01; }
+                if (sl[pos + 3] & 0x01) > 0 { sl[pos] += 1; } else { sl[pos] -= 1; }
+            }
+        }
+        // for y in 0..h {
+        //     for x in 0..w {
+        //         let pos = 4 * (y * w + x);
+        //         sl[pos + 2] += 1;
+        //         sl[pos + 1] += 1;
+        //         sl[pos] += 1;
+        //     }
+        // }
+    } else {
+        let mut xp: Vec<f32> = Vec::new();
+        let mut yp: Vec<f32> = Vec::new();
+
+
+        for i in 0.._n {
+            let val = w as f32 / 2.0 - ((j as f32 / (sl[y2 + 2 * i]) as f32).sin() * w as f32 / 2.0);
+            xp.push(val);
+            let val = h as f32 / 2.0 - ((j as f32 / sl[y2 + 2 * i + 1] as f32).cos() * h as f32 / 2.0);
+            yp.push(val);
+        }
+
+        for v in (0..byte_size).step_by(4) {
+            let y: usize = v / 4 / w;
+            let x: usize = v / 4 % w;
+            let mut s1 = 0.0;
+            let mut s2 = 0.0;
+            for i in 0.._n / 2 { s1 += ((xp[i] - x as f32) * (xp[i] - x as f32) + (yp[i] - y as f32) * (yp[i] - y as f32)).sqrt(); };
+            for i in _n / 2.._n { s2 += ((xp[i] - x as f32) * (xp[i] - x as f32) + (yp[i] - y as f32) * (yp[i] - y as f32)).sqrt(); };
+            let d = s1 / (s1 + s2);
+            sl[v + 3] = 255;
+            sl[v + 2] = sl[(d * 6400.0) as usize + byte_size];
+            sl[v + 1] = sl[(d * 17920.0) as usize + byte_size];
+            sl[v] = sl[(d * 11520.0) as usize + byte_size];
+        }
+    }
+}
---
+++ src/script.js
@@ -0,0 +1,208 @@
+let N = 5;
+let IsWasm = false;
+let first = true;
+let simpleMode = false;
+fetch('./pkg/wasm_vs_js_bg.wasm').then(response =>
+    response.arrayBuffer()
+).then(bytes =>
+    // the Rust side needs a cos function
+    WebAssembly.instantiate(bytes, {env: {cos: Math.cos}})
+).then(results => {
+    let module = {};
+    let mod = results.instance;
+    module.alloc = mod.exports.alloc;
+    module.dealloc = mod.exports.dealloc;
+    module.fill = mod.exports.fill;
+
+    class Animation {
+        constructor() {
+            this.cnv = null;
+            this.ctx = null;
+            this.bmp = null;
+            this.size = {w: 0, h: 0, cx: 0, cy: 0};
+            this.perf = 0;
+            this.total = 0;
+            this.j = 100000;
+            this.sin = [25600];
+            this.k = [N * 2];
+            this.XP = [N];
+            this.YP = [N];
+            this.bmp = null;
+            this.byteSize = 0;
+            this.pointer = null;
+            this.img = null;
+            this.timedelta = 0;
+            this.cur_n = 5;
+            this.wasm_box = null;
+            this.simple_box = null;
+
+        }
+
+        init() {                    //point of entry
+            this.createCanvas();
+            this.updateAnimation();
+        }
+
+        createCanvas() {
+            this.cnv = document.createElement("canvas");
+            this.ctx = this.cnv.getContext('2d', {willReadFrequently: true});
+            for (let i = 0; i < 25600; i++)
+                this.sin[i] = Math.round(128 + 127 * Math.sin(i / (2560 / N))); //create an array to speed up the work
+            for (let i = 0; i < N * 2; i++)
+                this.k[i] = 20 + Math.round(Math.random() * 40000) % 200;
+            this.setCanvasSize();
+            this.ctx.font = "30px Verdana";
+            this.ctx.fillStyle = "#FFFFFF80";
+            this.ctx.textAlign = "center";
+            document.body.appendChild(this.cnv);
+            this.cur_n = document.querySelector("#current_n");
+            this.wasm_box = document.querySelector("#wasm");
+            this.simple_box = document.querySelector("#simple");
+            IsWasm = this.wasm_box.checked;
+            simpleMode = this.simple_box.checked;
+            this.cur_n.value = N;
+
+            this.cur_n.addEventListener('change', () => {
+                first = true;
+                N = parseInt(this.cur_n.value);
+                this.XP.length = N;
+                this.YP.length = N;
+                this.perf = 0;
+                this.total = 0;
+                this.k.length = N * 2;
+                for (let i = 0; i < 25600; i++)
+                    this.sin[i] = Math.round(128 + 127 * Math.sin(i / (2560 / N))); //create an array to speed up the work
+                for (let i = 0; i < N * 2; i++)
+                    this.k[i] = 20 + Math.round(Math.random() * 40000) % 200;
+                this.saveArrToBmp();
+            });
+            this.wasm_box.addEventListener('change', () => {
+                IsWasm = this.wasm_box.checked;
+                this.perf = 0;
+                this.total = 0;
+                first = true;
+            });
+            this.simple_box.addEventListener('change', () => {
+                simpleMode = this.simple_box.checked;
+                this.perf = 0;
+                this.total = 0;
+                first = true;
+            });
+
+            window.addEventListener(`resize`, () => this.setCanvasSize());
+
+        }
+
+        saveArrToBmp() {
+            let y = this.size.w * 4 * this.size.h;
+            for (let i = 0; i < 25600; i++)
+                this.bmp[y + i] = this.sin[i];
+            y += 25600;
+            for (let i = 0; i < N * 2; i++)
+                this.bmp[i + y] = this.k[i];
+        }
+
+        setCanvasSize() {
+            this.size.w = this.cnv.width = window.innerWidth;
+            this.size.h = this.cnv.height = window.innerHeight;
+            let deltaH = Math.ceil(25800 / (this.size.w * 4));
+
+            this.byteSize = this.size.w * this.size.h * 4 + deltaH * this.size.w * 4;
+            this.pointer = module.alloc(this.byteSize);
+            this.bmp = new Uint8ClampedArray(mod.exports.memory.buffer, this.pointer, this.byteSize);
+            this.img = new ImageData(this.bmp, this.size.w, this.size.h + deltaH);
+
+            this.saveArrToBmp();
+
+            this.size.cx = this.size.w / 2;
+            this.size.cy = this.size.h / 2;
+            this.ctx.font = "30px Verdana";
+            this.ctx.fillStyle = "#FFFFFF80";
+            this.ctx.textAlign = "center";
+        }
+
+        updateCanvas() {
+            let W = this.size.w;
+            let H = this.size.h;
+            this.timedelta = performance.now();
+            this.j++;
+            if (!IsWasm) {
+                if (first) {
+                    for (let w = 0; w < N; w++) {
+                        this.XP[w] = W / 2 - (Math.sin(this.j / this.k[2 * w]) * W / 2);
+                        this.YP[w] = H / 2 - (Math.cos(this.j / this.k[2 * w + 1]) * H / 2);
+                    }
+
+                    let pos = 0;
+
+                    for (let Y = 0; Y < H; Y++) {
+                        for (let X = 0; X < W; X++) {
+                            let arr = [];
+                            for (let i = 0; i < N; i++)
+                                arr[i] = Math.sqrt((this.XP[i] - X) * (this.XP[i] - X) + (this.YP[i] - Y) * (this.YP[i] - Y));
+                            let S1 = 0, S2 = 0;
+                            let mid = Math.ceil(N / 2);
+                            for (let i = 0; i < N; i++) {
+                                if (i < mid) S1 += arr[i];
+                                S2 += arr[i];
+                            }
+                            let d = S1 / S2;
+                            pos = 4 * (Y * W + X);
+                            this.bmp[pos + 3] = 255;
+                            this.bmp[pos + 2] = this.sin[(d * 6400) | 0];
+                            this.bmp[pos + 1] = this.sin[(d * 17920) | 0];
+                            this.bmp[pos] = this.sin[(d * 11520) | 0];
+
+                        }
+                    }
+                    this.timedelta = performance.now() - this.timedelta;
+                    if (simpleMode) first = false;
+                } else {
+                    for (let Y = 0; Y < H; Y++) {
+                        for (let X = 0; X < W; X++) {
+                            let pos = 4 * (Y * W + X);
+                            if (this.bmp[pos + 2] === 255) this.bmp[pos + 3] = this.bmp[pos + 3]&0xFB;
+                            if (this.bmp[pos + 2] === 0) this.bmp[pos + 3] = this.bmp[pos + 3]|0x04;
+                            if ((this.bmp[pos + 3]&0x04)>0) this.bmp[pos + 2] += 1;
+                            else this.bmp[pos + 2] -= 1;
+                            if (this.bmp[pos + 1] === 255) this.bmp[pos + 3] = this.bmp[pos + 3]&0xFD;
+                            if (this.bmp[pos + 1] === 0) this.bmp[pos + 3] = this.bmp[pos + 3]|0x02;
+                            if ((this.bmp[pos + 3]&0x02)>0) this.bmp[pos + 1] += 1;
+                            else this.bmp[pos + 1] -= 1;
+                            if (this.bmp[pos] === 255) this.bmp[pos + 3] = this.bmp[pos + 3]&0xFE;
+                            if (this.bmp[pos] === 0) this.bmp[pos + 3] = this.bmp[pos + 3]|0x01;
+                            if ((this.bmp[pos + 3]&0x01)>0) this.bmp[pos] += 1;
+                            else this.bmp[pos] -= 1;
+                        }
+                    }
+                    this.timedelta = performance.now() - this.timedelta;
+                }
+            } else {
+                this.timedelta = performance.now();
+                module.fill(this.pointer, this.size.w, this.size.h, N, this.j, simpleMode);
+                //module.fill(this.pointer, this.size.w, this.size.h, this.total);
+                this.timedelta = performance.now() - this.timedelta;
+            }
+            //this.ss+=0.01;
+            //if (this.ss>20) this.ss=1.0;
+            this.ctx.putImageData(this.img, 0, 0);
+        }
+
+        updateAnimation() {
+            //let time = performance.now();
+            this.updateCanvas();
+
+            window.requestAnimationFrame(() => this.updateAnimation());
+            this.perf += this.timedelta;
+            this.total++;
+            this.ctx.fillStyle = "#FFFFFF80";
+            this.ctx.fillText('Framing time = ' + Math.round(this.perf / this.total).toFixed(2) + ' milliseconds', this.size.cx, this.size.cy);
+            this.ctx.fillStyle = "#00000080";
+            this.ctx.fillText('Framing time = ' + Math.round(this.perf / this.total).toFixed(2) + ' milliseconds', this.size.cx, this.size.cy+40);
+        }
+    }
+
+    window.onload = () => {
+        new Animation().init();
+    }
+});
